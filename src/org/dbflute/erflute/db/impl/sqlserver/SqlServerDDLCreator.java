f8a1d22eca5a5227a803887804b63032e8271799
しゃってん <schatten4810@gmail.com>
2017-06-23 13:29:48 +0900

    SQLServer column comment support.

--- src/org/dbflute/erflute/db/impl/sqlserver/SqlServerDDLCreator.java
+++ src/org/dbflute/erflute/db/impl/sqlserver/SqlServerDDLCreator.java
@@ -1,10 +1,16 @@
 package org.dbflute.erflute.db.impl.sqlserver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.dbflute.erflute.core.util.Check;
 import org.dbflute.erflute.db.impl.db2.tablespace.DB2TablespaceProperties;
 import org.dbflute.erflute.editor.model.ERDiagram;
 import org.dbflute.erflute.editor.model.dbexport.ddl.DDLCreator;
+import org.dbflute.erflute.editor.model.diagram_contents.element.node.table.ERTable;
+import org.dbflute.erflute.editor.model.diagram_contents.element.node.table.column.ERColumn;
 import org.dbflute.erflute.editor.model.diagram_contents.element.node.table.column.NormalColumn;
+import org.dbflute.erflute.editor.model.diagram_contents.not_element.group.ColumnGroup;
 import org.dbflute.erflute.editor.model.diagram_contents.not_element.sequence.Sequence;
 import org.dbflute.erflute.editor.model.diagram_contents.not_element.tablespace.Tablespace;
 
@@ -98,5 +104,83 @@ public class SqlServerDDLCreator extends DDLCreator {
         }
 
         return ddl.toString();
+    }
+
+    @Override
+    protected List<String> doBuildCreateComment(ERTable table) {
+        final List<String> ddlList = new ArrayList<>();
+
+        final String tableComment = this.filterComment(table.getLogicalName(), table.getDescription(), false);
+
+        if (!Check.isEmpty(tableComment)) {
+            final StringBuilder ddl = new StringBuilder();
+
+            ddl.append("EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'");
+            ddl.append(tableComment.replaceAll("'", "''"));
+            ddl.append("', @level0type=N'schema', @level0name=N'");
+            ddl.append("dbo");
+            ddl.append("', @level1type=N'table', @level1name=N'");
+            ddl.append(filter(table.getPhysicalName()));
+            ddl.append("'");
+            if (this.semicolon) {
+                ddl.append(";");
+            }
+
+            ddlList.add(ddl.toString());
+        }
+
+        for (final ERColumn column : table.getColumns()) {
+            if (column instanceof NormalColumn) {
+                final NormalColumn normalColumn = (NormalColumn) column;
+
+                final String comment = this.filterComment(normalColumn.getLogicalName(), normalColumn.getDescription(), true);
+
+                if (!Check.isEmpty(comment)) {
+                    final StringBuilder ddl = new StringBuilder();
+
+                    ddl.append("EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'");
+                    ddl.append(comment.replaceAll("'", "''"));
+                    ddl.append("', @level0type=N'schema', @level0name=N'");
+                    ddl.append("dbo");
+                    ddl.append("', @level1type=N'table', @level1name=N'");
+                    ddl.append(filter(table.getPhysicalName()));
+                    ddl.append("', @level2type=N'column' , @level2name=N'");
+                    ddl.append(filter(normalColumn.getPhysicalName()));
+                    ddl.append("'");
+                    if (this.semicolon) {
+                        ddl.append(";");
+                    }
+
+                    ddlList.add(ddl.toString());
+                }
+            } else {
+                final ColumnGroup columnGroup = (ColumnGroup) column;
+
+                for (final NormalColumn normalColumn : columnGroup.getColumns()) {
+                    final String comment = this.filterComment(normalColumn.getLogicalName(), normalColumn.getDescription(), true);
+
+                    if (!Check.isEmpty(comment)) {
+                        final StringBuilder ddl = new StringBuilder();
+
+                        ddl.append("EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'");
+                        ddl.append(comment.replaceAll("'", "''"));
+                        ddl.append("', @level0type=N'schema', @level0name=N'");
+                        ddl.append("dbo");
+                        ddl.append("', @level1type=N'table', @level1name=N'");
+                        ddl.append(filter(table.getPhysicalName()));
+                        ddl.append("', @level2type=N'column' , @level2name=N'");
+                        ddl.append(filter(normalColumn.getPhysicalName()));
+                        ddl.append("'");
+                        if (this.semicolon) {
+                            ddl.append(";");
+                        }
+
+                        ddlList.add(ddl.toString());
+                    }
+                }
+            }
+        }
+
+        return ddlList;
     }
 }
